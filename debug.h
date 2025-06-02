1ac588a25e6d986c4db4e8fcc4c58450ca6b93df
Kyohei <yamaguky@oregonstate.edu>
2025-06-02 16:21:21 -0700

    Remove unused files

--- debug.h
+++
@@ -1,5 +0,0 @@
-#ifndef DEBUG_H
-#define DEBUG_H
-extern void dump_registers(void);
-extern void dump_backtrace(void);
-#endif
--- dump_backtrace.s
+++
@@ -1,9 +0,0 @@
-.globl dump_backtrace
-.type dump_backtrace, @function
-dump_backtrace:
-ret
-
-
-.section .rodata
-backtrace_format_str:
-.asciz "%3ld: [%lx] %s () %s\n"
