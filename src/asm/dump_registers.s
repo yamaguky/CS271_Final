9121a18236e0294e305fe78c1a80c1e4ca80db8c
Christopher Vote <cbourjaily@gmail.com>
2025-06-06 16:26:20 -0700

    Wrote a draft of the README.md, removed excess comments on register_dump.s, included README.tx version.

--- src/asm/dump_registers.s
+++ src/asm/dump_registers.s
@@ -6,25 +6,25 @@ dump_registers:
     subq $128, %rsp		# Allocates 128 bytes on the stack to store registers
 
     # Save caller-saved and callee-saved general-purpose registers
-    movq %rax,   0(%rsp)	# Save %rax
-    movq %rbx,   8(%rsp)	# Save %rbx
-    movq %rcx,  16(%rsp)	# Save %rcx
-    movq %rdx,  24(%rsp)	# Save %rdx
-    movq %rsi,  32(%rsp)	# Save %rsi
-    movq %rdi,  40(%rsp)	# Save %rdi
-    movq %rbp,  48(%rsp)	# Save %rbp
+    movq %rax,   0(%rsp	
+    movq %rbx,   8(%rsp)
+    movq %rcx,  16(%rsp)
+    movq %rdx,  24(%rsp)
+    movq %rsi,  32(%rsp)
+    movq %rdi,  40(%rsp)
+    movq %rbp,  48(%rsp)
 
     leaq 8(%rbp), %rax    	# Load return address into %rax at [%rbp + 8]
     movq %rax,  56(%rsp)	# Save return address
 
-    movq %r8,   64(%rsp)	# Save %r8
-    movq %r9,   72(%rsp)	# Save %r9
-    movq %r10,  80(%rsp)	# Save %r10
-    movq %r11,  88(%rsp)	# Save %r11
-    movq %r12,  96(%rsp)	# Save %r12
-    movq %r13, 104(%rsp)	# Save %r13
-    movq %r14, 112(%rsp)	# Save %r14
-    movq %r15, 120(%rsp)	# Save %r15
+    movq %r8,   64(%rsp)
+    movq %r9,   72(%rsp)
+    movq %r10,  80(%rsp)
+    movq %r11,  88(%rsp)
+    movq %r12,  96(%rsp)
+    movq %r13, 104(%rsp)
+    movq %r14, 112(%rsp)
+    movq %r15, 120(%rsp)
 
     # Now call the C function with pointer to register dump
     movq %rsp, %rdi		# Pass pointer to saved registers as argument
