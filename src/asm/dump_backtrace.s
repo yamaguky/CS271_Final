a94841f933d964febd27d51529100a55082d0cbe
Christopher Vote <cbourjaily@gmail.com>
2025-06-06 01:52:19 -0700

    Updated the directory with the recent files shared on Discord by Abdul

--- src/asm/dump_backtrace.s
+++ src/asm/dump_backtrace.s
@@ -1,11 +1,10 @@
-.globl  _dump_backtrace
-.type   _dump_backtrace, @function
+.globl _dump_backtrace
+.type  _dump_backtrace, @function
 
-# Constants (see `man dladdr` for Dl_info layout) 
-#https://man7.org/linux/man-pages/man3/dladdr.3.html
-.equ    MAX_FRAMES,   64   # hard cap on walked frames           
-.equ    DLINFO_SZ,    32   # sizeof(struct Dl_info) -> see dlfcn.h
-.equ    SLOT_SZ,      48   # DLINFO_SZ + 16 for guaranteed 16-B alignment
+# Constants
+.equ    MAX_FRAMES,   64   # Max stack frames to walk
+.equ    DLINFO_SZ,    32   # sizeof(struct Dl_info)
+.equ    SLOT_SZ,      48   # DLINFO_SZ + 16 for alignment
 
 _dump_backtrace:
         pushq   %rbp
@@ -15,10 +14,9 @@ _dump_backtrace:
         pushq   %r13
         pushq   %r14
 
-        xorq    %r14, %r14            #depth counter                      
-        movq    (%rbp), %rbx          #current frame pointer              
-        leaq    -1(%rsp), %r13        #prevent seg fault
-
+        xorq    %r14, %r14            # depth = 0
+        movq    (%rbp), %rbx          # current frame pointer
+        leaq    -1(%rsp), %r13        # for segfault guard
 
 .Loop:
         cmpq    $MAX_FRAMES, %r14
@@ -28,26 +26,26 @@ _dump_backtrace:
         cmpq    %r13, %rbx
         jb      .Done
 
-        movq    (%rbx), %r12          # parent frame pointer              
+        movq    (%rbx), %r12          # parent frame pointer
         testq   %r12, %r12
         je      .Done
 
-        movq    8(%rbx), %rdi         # return address -> rdi             
-        subq    $SLOT_SZ, %rsp        # reserve one aligned Dl_info slot  
-        leaq    (%rsp), %rsi          # rsi = &Dl_info                    
-        call    dladdr@PLT            # see `man dladdr`                  
+        movq    8(%rbx), %rdi         # return address → rdi
+        subq    $SLOT_SZ, %rsp        # reserve stack for Dl_info
+        leaq    (%rsp), %rsi          # rsi = &Dl_info
+        call    dladdr@PLT
 
-        # populate printf args
-        movq    24(%rsp), %rdx        # symbol address (dli_saddr)        
-        movq    16(%rsp), %rcx        # symbol name   (dli_sname)         
-        movq     0(%rsp), %r8         # image name    (dli_fname)         
-        
+        # printf("%3ld: [%lx] %s () %s\n", depth, ret_addr, symbol name, image name)
+        movq    24(%rsp), %rdx        # dli_saddr
+        movq    16(%rsp), %rcx        # dli_sname
+        movq     0(%rsp), %r8         # dli_fname
+
         leaq    backtrace_format_str(%rip), %rdi
-        movq    %r14, %rsi            # depth index                       
-        xorq    %r9, %r9
+        movq    %r14, %rsi            # depth
+        xorq    %r9, %r9              # clear r9 (not used)
         call    printf@PLT
 
-        addq    $SLOT_SZ, %rsp        # release Dl_info slot              
+        addq    $SLOT_SZ, %rsp        # deallocate Dl_info
 
         incq    %r14
         movq    %r12, %rbx
